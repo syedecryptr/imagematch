df87c7cc508ec4ef1c9c7e5d556835654c569bdc
afshan.syed <afshan.syed@tcs.com>
2020-06-03 00:58:14 +0530

    resolving db issue and changing back to old condition as new failed

--- js/gamelogic/config.js
+++ js/gamelogic/config.js
@@ -14,4 +14,4 @@ var marginValue = Math.ceil(1/(6*numberOfColumns)*100)+"%"
 
 //this time is in miliseconds
 var flashScreenTIme=1000*(numberOfColumns+2)
-var time_delay_for_db = 50
+var time_delay_for_db = 500
